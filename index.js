20076916a3f8ca777a44dcb96fff09c83389e67e
huuvuong09 <41174978+huuvuong09@users.noreply.github.com>
2025-04-23 21:45:25 +0700

    bài tập

---
+++ index.js
@@ -0,0 +1,53 @@
+// var fullName = "Vượng";
+// let age = 12;
+// const handsome = false;
+// var school = "Phúc Yên";
+// var code = 100;
+// let price = 1000;
+// price = 15000;
+// code = 300;
+// console.log(number1 + numnber2);
+// console.log(number1 - numnber2);
+// console.log(number1 * numnber2);
+// console.log(number1 / numnber2);
+// console.log(number1 % numnber2);
+// console.log(number1 ** numnber2);
+// let number1 = 18;
+// let numnber2 = 100;
+// let number3 = 2.3;
+// let age = prompt("nhập tuổi")
+// console.log(age);
+// let number1 = +prompt("nhấp số 1");
+// let number2 = +prompt("nhấp số 2");
+// let sum = number1 + number2;
+// console.log (sum);
+// bài 1
+let width = +prompt("nhập độ rộng");
+let height = +prompt("nhập độ dài");
+let sum = width + height;
+console.log(sum);
+//bài 2
+let firstNumber = +prompt("nhấp số 1");
+ let secondNumber = +prompt("nhấp số 2");
+ let tong = firstNumber + secondNumber;
+alert(tong);
+//bài 3
+// let balance = 25.000;
+//bài 4
+let math = +prompt("nhập điểm môn toán");
+let physics = +prompt("nhập điểm môn lý");
+let chemistry = +prompt("nhập điểm môn văn");
+let sum2 = (math + physics + chemistry) / 3 ;
+console.log("Điểm trung bình = " + sum2);
+
+
+
+
+
+
+
+
+
+
+
+
